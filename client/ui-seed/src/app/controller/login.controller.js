4d43048da2c9593b39a25e042e6609cbe0dc7719
DIVYASHREE VIJAYKUMAR <divya@DIVYASHREEs-MacBook-Pro.local>
2016-05-08 13:30:05 -0400

    feat(init):ui-movieflix

---
+++ client/ui-seed/src/app/controller/login.controller.js
@@ -0,0 +1,27 @@
+/**
+ * Created by divya on 5/3/16.
+ */
+
+/**
+ * Created by divya on 5/3/16.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('movieFlix', ['ngMessages'])
+        .controller('loginController', loginController);
+
+    loginController.$inject = ['loginService'];
+
+    function loginController(loginService){
+        var loginVm = this;
+
+        loginVm.processLoginForm = processLoginForm;
+
+
+        function processLoginForm(){
+            loginService.submitLoginForm(loginVm.login.emailid,loginVm.login.password);
+        }
+    }
+})();
